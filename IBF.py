33cbdc66c76ddbc77247ef27b3d7242a4ccb712a
ripcy <arifripcy@hotmail.com>
2022-09-29 19:39:03 +0900

    29092022

--- IBF.py
+++ IBF.py
@@ -24,7 +24,9 @@ folderIBF = ("D:/TAHUN "+thnskr+"/"+str (bulanangka)+". "+bulan+"/IBF/")
 src = "https://web.meteo.bmkg.go.id//media/data/bmkg/ibfnew/34_papua_24.png"
 urllib.request.urlretrieve(src, folderIBF+bsk+".png")
 image = Image.open(folderIBF+bsk+".png")
+image2 = Image.open(folderIBF+bsk+".png")
 narasi = ("Informasi Prakiraan Cuaca Berbasis Dampak wilayah Provinsi Papua berlaku "+tanggalbesok+" pukul 09.00 WIT - "+tanggallusa+" pukul 09.00 WIT Informasi lebih lengkap dapat diakses pada : http://signature.bmkg.go.id/")
+
 
 ### Passing File to Clipbord
 output = BytesIO()
@@ -41,15 +43,44 @@ chromeOptions.add_argument("user-data-dir=" + currentPath + "cookies")
 #chromeOptions.add_argument("--headless")
 driver = webdriver.Chrome(driverPath, options=chromeOptions)
 
-driver.get("http://web.whatsapp.com")
-WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//*[@id="side"]/div[1]/div/div/div[2]/div/div[2]'))).send_keys("Arif", Keys.ENTER)
-WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'))).send_keys(Keys.CONTROL, 'v')
-WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[1]/div[3]/div/div/div[2]/div[1]/div[2]'))).send_keys(narasi)
-WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]/div'))).click()
-driver.execute_script("window.open('');")
-driver.switch_to.window(driver.window_handles[1])
-driver.get("https://web.telegram.org/")
-time.sleep(20)
+#if len(nomor) == 0:
+#    driver.get("http://web.whatsapp.com")
+#else:
+#    driver.get("https://web.whatsapp.com/send?phone="+nomor+"&text&type=phone_number&app_absent=1")
+
+#driver.get("http://web.whatsapp.com")
+#WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//*[@id="side"]/div[1]/div/div/div[2]/div/div[2]'))).send_keys("Arif", Keys.ENTER)
+#WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'))).send_keys(Keys.CONTROL, 'v')
+#WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[1]/div[3]/div/div/div[2]/div[1]/div[2]'))).send_keys(narasi)
+#WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]/div'))).click()
+#driver.execute_script("window.open('');")
+#driver.switch_to.window(driver.window_handles[1])
+#driver.get("https://web.telegram.org/")
+#WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//*[@id="folders-container"]/div/div[1]/ul/a[1]/div[1]'))).click()
+#WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//*[@id="column-center"]/div/div/div[4]/div/div[1]/div/div[8]/div[1]/div[1]'))).click()
+#WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//*[@id="column-center"]/div/div/div[4]/div/div[1]/div/div[8]/div[1]/div[1]'))).send_keys(Keys.CONTROL, 'v')
+#WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[5]/div/div[3]/div[1]'))).send_keys(narasi)
+#WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[5]/div/div[1]/button'))).click()
+
+import requests
+
+url = "https://api.telegram.org/bot5663329448:AAFYtirH9HbrgcsJWPisd-blu-AL1jV0Aes/sendPhoto"
+
+payload = {
+    "photo": image,
+    "caption": narasi,
+    "disable_notification": False,
+    "reply_to_message_id": None
+}
+headers = {
+    "accept": "application/json",
+    "content-type": "application/json"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.text)
+
 
 '''
 #time.sleep(5)
@@ -62,4 +93,4 @@ time.sleep(20)
 #time.sleep(10)
 '''
 
-driver.close()
+driver.quit()
